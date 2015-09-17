181978ad06ad3e883d9a98b6dae0a2df993f37a1
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-17 09:42:50 -0500

    code that I never commited since 2012-01-29

--- lib/App/Goosebumper/Site/UH/BlackboardVista.pm
+++ lib/App/Goosebumper/Site/UH/BlackboardVista.pm
@@ -7,8 +7,12 @@ use App::Goosebumper::SiteHelper;
 
 use Log::Log4perl qw(:easy);
 
+use WWW::Scripter;
+
 use Data::Dumper;
 use Carp;
+
+my $url = "http://www.uh.edu/blackboard/";
 
 sub new {
 	my ($class, $site_helper) = @_;
@@ -27,9 +31,41 @@ sub screech {
 	my $site_name = $self->{site_name};
 	DEBUG "Entering $site_name";
 
+	my $mech = new WWW::Scripter;
+	$self->{_mech} = $mech;
+	$mech->use_plugin(JavaScript =>
+		engine  => 'JE',
+	);
+
+	$mech->get( $url );
+
+	$self->_login();
+
+	#$self->_visit_courses();
+	#$site_helper->download_cache($self);
+	#$site_helper->write_cache($self->{cache});
+}
+
+sub _login {
+	my $self = shift;
+	my $site_helper = $self->{_SiteHelper};
+	my $site_name = $self->{site_name};
+	my $mech = $self->{_mech};
+
 	my $cred = $site_helper->{config}{site}{$site_name};
-	#{ local $Data::Dumper::Indent = 0; local $Data::Dumper::Terse = 1;
-	#DEBUG "We have the credentials ", Dumper $cred; }
+
+	$mech->follow_link( url_regex => qr/logonDisplay/ );
+
+	$mech->submit_form( with_fields => {
+		webctid => $cred->{username},
+		password => $cred->{password},
+	});
+	$mech->post( URI->new_abs("/webct/authenticateUser.dowebct", $mech->base),
+		$mech->current_form);
+	#$mech->get('https://learn.uh.edu/webct/urw/tp0.lc5122011/cobaltMainFrame.dowebct');
+	DEBUG $mech->title;
+	DEBUG "Logged in";
+	print $mech->content;
 }
 
 1;
